2ff3109d955fc371101f324016d5c265bca98187
matasr <batassp@gmail.com>
2017-05-12 17:40:38 +0300

    Main BG inserted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    CardComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/text/text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.css']
+})
+export class TextComponent {
+
+	
+
+}
